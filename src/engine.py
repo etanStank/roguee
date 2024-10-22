96b847918b897811c7f858efdf060e87c5f10fa8
John <20042@jhc.school.nz>
2024-10-23 10:45:26 +1300

    Part 3-4
    Completed parts 3-4.

--- src/engine.py
+++ src/engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -12,20 +13,35 @@ class Engine:
         self.entities = entities
         self.event_handler = event_handler
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events:Iterable[Any]) -> None:
-        for even in events:
+        for event in events:
             action = self.event_handler.dispatch(event)
 
             if action is None:
                 continue
         
             action.perform(self, self.player)
+
+            self.update_fov()
+        
+    def update_fov(self) -> None:
+            self.game_map.visible[:] = compute_fov(
+                 self.game_map.tles["transparent"],
+                 (self.player.x, self.player.y),
+                 radius = 8,
+                 light_walls = True,
+            )
+
+            self.game_map.explored |= self.game_map.visible
             
             def render(self, console: Console, context: Context) -> None:
                  self.game_map.render(console)
                  
                  for entity in self.entities:
-                      console.print(entity.x, entity.y, entity.z, entity.char, fg=entity.color)
-                Context.present(Console)
-                Console.clear()
+                      if self.game_map.visible[entity.x, entity.y]:
+                           console.print(entity.x, entity.y, entity.char, fg = entity.color)
+
+            Context.present(Console)
+            Console.clear()
